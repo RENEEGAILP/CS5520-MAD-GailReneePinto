11c602c4dd6a5747d220de543bfcd9b256c7fcc5
RENEEGAILP <22723224+RENEEGAILP@users.noreply.github.com>
2021-05-28 16:06:11 -0400

    Added new Activity clicky_click Modified onClick button

---
+++ NUMAD21Su-GailReneePinto/app/src/main/java/edu/neu/madcourse/numad21su_gailreneepinto/ClickyClickyActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21su_gailreneepinto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickyClickyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky_clicky);
+    }
+}

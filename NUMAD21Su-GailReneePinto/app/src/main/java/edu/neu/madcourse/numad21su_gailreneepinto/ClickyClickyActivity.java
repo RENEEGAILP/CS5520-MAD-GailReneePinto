84013e7da6b14d53ab0196deb876dc50ae959118
RENEEGAILP <22723224+RENEEGAILP@users.noreply.github.com>
2021-05-28 16:56:54 -0400

    Added code for button click

--- NUMAD21Su-GailReneePinto/app/src/main/java/edu/neu/madcourse/numad21su_gailreneepinto/ClickyClickyActivity.java
+++ NUMAD21Su-GailReneePinto/app/src/main/java/edu/neu/madcourse/numad21su_gailreneepinto/ClickyClickyActivity.java
@@ -3,6 +3,9 @@ package edu.neu.madcourse.numad21su_gailreneepinto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ClickyClickyActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class ClickyClickyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky_clicky);
+        findViewById(R.id.button_A).setOnClickListener(this::onClick);
+        findViewById(R.id.button_B).setOnClickListener(this::onClick);
+        findViewById(R.id.button_C).setOnClickListener(this::onClick);
+        findViewById(R.id.button_D).setOnClickListener(this::onClick);
+        findViewById(R.id.button_E).setOnClickListener(this::onClick);
+        findViewById(R.id.button_F).setOnClickListener(this::onClick);
+
     }
+
+    public void onClick(View view)
+    {
+        TextView textView = findViewById(R.id.pressed_textView);
+        Button button_pressed = findViewById(view.getId());
+        textView.setText("Button Pressed : " + button_pressed.getText().toString());
+        textView.setVisibility(View.VISIBLE);
+    }
+
+
 }

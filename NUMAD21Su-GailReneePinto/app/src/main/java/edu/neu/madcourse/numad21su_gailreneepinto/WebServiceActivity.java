ff0849cc2bb99e0e6d689a492f2da3d812453b17
RENEEGAILP <22723224+RENEEGAILP@users.noreply.github.com>
2021-06-25 11:14:04 -0400

    Added UI components for At your service

--- NUMAD21Su-GailReneePinto/app/src/main/java/edu/neu/madcourse/numad21su_gailreneepinto/WebServiceActivity.java
+++ NUMAD21Su-GailReneePinto/app/src/main/java/edu/neu/madcourse/numad21su_gailreneepinto/WebServiceActivity.java
@@ -2,13 +2,30 @@ package edu.neu.madcourse.numad21su_gailreneepinto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class WebServiceActivity extends AppCompatActivity {
+
+    ImageView HTTPResponseImageView;
+    Button loadDataButton;
+    EditText HTTPStatusCodeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_web_service );
+        HTTPResponseImageView = findViewById( R.id.http_image );
+        loadDataButton = findViewById( R.id.HTTP_status_load_image_button );
+        HTTPStatusCodeEditText = findViewById( R.id.HTTP_status_code_input_editText );
+    }
+
+    protected void onLoadDataButtonClick(View view)
+    {
+
     }
 }
